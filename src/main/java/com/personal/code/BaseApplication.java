4deac9d1a2968073d8f8825f332d5492e4dc69a3
zhengqiang <zhengqiang@shuidihuzhu.com>
2019-05-16 16:10:19 +0800

    -init
    Signed-off-by: zhengqiang <zhengqiang@shuidihuzhu.com>

---
+++ src/main/java/com/personal/code/BaseApplication.java
@@ -0,0 +1,7 @@
+package com.personal.code;
+
+public class BaseApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/com/personal/code/BaseApplicationTest.java
@@ -0,0 +1,10 @@
+package com.personal.code;
+
+import org.junit.Test;
+
+
+public class BaseApplicationTest {
+
+    @Test
+    public void test() {}
+}
