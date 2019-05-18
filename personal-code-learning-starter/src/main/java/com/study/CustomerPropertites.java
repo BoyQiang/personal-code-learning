a32f6942f7855cb0c06b5c654b986abad528a3ef
zhengqiang <zhengqiang@shuidihuzhu.com>
2019-05-18 23:20:49 +0800

    update .gitignore
    Signed-off-by: zhengqiang <zhengqiang@shuidihuzhu.com>

--- personal-code-learning-starter/src/main/java/com/study/CustomerPropertites.java
+++ personal-code-learning-starter/src/main/java/com/study/CustomerPropertites.java
@@ -2,10 +2,7 @@ package com.study;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
 @ConfigurationProperties(prefix="person")
 @Data
 public class CustomerPropertites {
